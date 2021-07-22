771d50da65fd77c6293b0327ce15bb190063a988
IrinaPastea <87833809+IrinaPastea@users.noreply.github.com>
2021-07-23 00:40:25 +0300

    Add files via upload
    First Version of the project

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Total_Commander
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb_partitie_1 = new System.Windows.Forms.ComboBox();
+            this.lv_locatie_1 = new System.Windows.Forms.ListView();
+            this.tb_path_1 = new System.Windows.Forms.TextBox();
+            this.lv_locatie_2 = new System.Windows.Forms.ListView();
+            this.cb_partitie_2 = new System.Windows.Forms.ComboBox();
+            this.tb_path_2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cb_partitie_1
+            // 
+            this.cb_partitie_1.FormattingEnabled = true;
+            this.cb_partitie_1.Location = new System.Drawing.Point(0, 0);
+            this.cb_partitie_1.Name = "cb_partitie_1";
+            this.cb_partitie_1.Size = new System.Drawing.Size(49, 23);
+            this.cb_partitie_1.TabIndex = 0;
+            this.cb_partitie_1.SelectedIndexChanged += new System.EventHandler(this.cb_partitions_1_SelectedIndexChanged);
+            // 
+            // lv_locatie_1
+            // 
+            this.lv_locatie_1.HideSelection = false;
+            this.lv_locatie_1.Location = new System.Drawing.Point(0, 56);
+            this.lv_locatie_1.Name = "lv_locatie_1";
+            this.lv_locatie_1.Size = new System.Drawing.Size(121, 97);
+            this.lv_locatie_1.TabIndex = 1;
+            this.lv_locatie_1.UseCompatibleStateImageBehavior = false;
+            this.lv_locatie_1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lv_locatie_1_MouseDoubleClick);
+            // 
+            // tb_path_1
+            // 
+            this.tb_path_1.Location = new System.Drawing.Point(0, 27);
+            this.tb_path_1.Name = "tb_path_1";
+            this.tb_path_1.ReadOnly = true;
+            this.tb_path_1.Size = new System.Drawing.Size(361, 23);
+            this.tb_path_1.TabIndex = 2;
+            this.tb_path_1.TextChanged += new System.EventHandler(this.tb_path_1_TextChanged);
+            // 
+            // lv_locatie_2
+            // 
+            this.lv_locatie_2.HideSelection = false;
+            this.lv_locatie_2.Location = new System.Drawing.Point(975, 56);
+            this.lv_locatie_2.Name = "lv_locatie_2";
+            this.lv_locatie_2.Size = new System.Drawing.Size(121, 97);
+            this.lv_locatie_2.TabIndex = 3;
+            this.lv_locatie_2.UseCompatibleStateImageBehavior = false;
+            this.lv_locatie_2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lv_locatie_2_MouseDoubleClick);
+            // 
+            // cb_partitie_2
+            // 
+            this.cb_partitie_2.FormattingEnabled = true;
+            this.cb_partitie_2.Location = new System.Drawing.Point(1047, 0);
+            this.cb_partitie_2.Name = "cb_partitie_2";
+            this.cb_partitie_2.Size = new System.Drawing.Size(49, 23);
+            this.cb_partitie_2.TabIndex = 4;
+            this.cb_partitie_2.SelectedIndexChanged += new System.EventHandler(this.cb_partitie_2_SelectedIndexChanged);
+            // 
+            // tb_path_2
+            // 
+            this.tb_path_2.Location = new System.Drawing.Point(735, 27);
+            this.tb_path_2.Name = "tb_path_2";
+            this.tb_path_2.ReadOnly = true;
+            this.tb_path_2.Size = new System.Drawing.Size(361, 23);
+            this.tb_path_2.TabIndex = 5;
+            this.tb_path_2.TextChanged += new System.EventHandler(this.tb_path_2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 686);
+            this.Controls.Add(this.tb_path_2);
+            this.Controls.Add(this.cb_partitie_2);
+            this.Controls.Add(this.lv_locatie_2);
+            this.Controls.Add(this.tb_path_1);
+            this.Controls.Add(this.lv_locatie_1);
+            this.Controls.Add(this.cb_partitie_1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Double File Explorer Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cb_partitie_1;
+        private System.Windows.Forms.ListView lv_locatie_1;
+        private System.Windows.Forms.TextBox tb_path_1;
+        private System.Windows.Forms.ListView lv_locatie_2;
+        private System.Windows.Forms.ComboBox cb_partitie_2;
+        private System.Windows.Forms.TextBox tb_path_2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Total_Commander
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lv_adauga_coloane(ListView lv)
+        {
+            lv.Columns.Add("Name", (int)(lv.Width * 0.5));
+            lv.Columns.Add("Ext", (int)(lv.Width * 0.1));
+            lv.Columns.Add("Size", (int)(lv.Width * 0.2));
+            lv.Columns.Add("Date", (int)(lv.Width * 0.2));
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Aflarea dimensiunilor ecranului pentru a putea baza elementele din form pe procentaje din dimensiuni
+
+            int latimeEcran = Screen.PrimaryScreen.Bounds.Width;
+            int inaltimeEcran = Screen.PrimaryScreen.Bounds.Height;
+
+            Width = (int)(latimeEcran * 0.75);
+            Height = (int)(inaltimeEcran * 0.75);
+
+            Location = new Point((int)(latimeEcran * 0.125), (int)(inaltimeEcran * 0.125));
+
+
+            // Locatia si Dimensiunile primului listview si combobox-ului cu partitiile disponibile si textbox-ul path-ului
+
+            lv_locatie_1.Width = Width / 2 - 2;
+            lv_locatie_1.Height = (int)(Height * 0.9);
+            lv_locatie_1.Location = new Point(2, (int)(Height * 0.1));
+
+            cb_partitie_1.Location = new Point(2, (int)(Height * 0.047));
+            tb_path_1.Location = new Point(2, (int)(Height * 0.075));
+
+            //Locatia si Dimensiunile la al doilea listview si combobox cu partitiile si textbox-ul path-ul
+
+            lv_locatie_2.Width = Width / 2 - 2;
+            lv_locatie_2.Height = (int)(Height * 0.9);
+            lv_locatie_2.Location = new Point(Width / 2 + 2, (int)(Height * 0.1));
+
+            cb_partitie_2.Location = new Point(Width / 2 + 2, (int)(Height * 0.047));
+            tb_path_2.Location = new Point(Width / 2 + 2, (int)(Height * 0.075));
+
+            // Adaugarea numelor partitiilor in combobox-urile de selectare a partitiilor disponibile
+
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                cb_partitie_1.Items.Add(d.Name);
+                cb_partitie_2.Items.Add(d.Name);
+            }
+
+            cb_partitie_1.SelectedIndex = 0;
+            cb_partitie_2.SelectedIndex = 1;
+
+            lv_locatie_1.View = View.Details;
+            lv_locatie_2.View = View.Details;
+
+            lv_adauga_coloane(lv_locatie_1);
+            lv_adauga_coloane(lv_locatie_2);
+
+
+        }
+
+
+        private void cb_partitions_1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            tb_path_1.Text = cb_partitie_1.Text;
+
+        }
+
+        private void cb_partitie_2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            tb_path_2.Text = cb_partitie_2.Text;
+
+        }
+
+        private void tb_path_1_TextChanged(object sender, EventArgs e)
+        {
+
+            // Stergem toate itemele din listview atunci cand selectam sa vedem o alta partitie
+
+            lv_locatie_1.Items.Clear();
+
+
+            // Adaugam imaginea pentru foldere si pentru itemul de mers inapoi cu un folder
+
+            var imageList = new ImageList();
+
+            imageList.Images.Add("folderImage", Image.FromFile("folderIcon.ico"));
+            imageList.Images.Add("gobackfolderImage", Image.FromFile("gobackfolderIcon.png"));
+
+            ListViewItem item_for_image;
+
+            if (Directory.GetParent(tb_path_1.Text) != null)
+            {
+                var itemul_mers_inapoi = new ListViewItem(new[] { "[...]", "<DIR>", "", "" });
+                item_for_image = lv_locatie_1.Items.Add(itemul_mers_inapoi);
+                item_for_image.ImageIndex = imageList.Images.IndexOfKey("gobackfolderImage");
+            }
+
+            imageList.ColorDepth = ColorDepth.Depth32Bit;
+            lv_locatie_1.SmallImageList = imageList;
+
+
+            // Obtinem o lista cu toate folderele si o lista cu toate fisierele
+
+            string[] toateFolderele = Directory.GetDirectories(tb_path_1.Text);
+            string[] toateFisierele = Directory.GetFiles(tb_path_1.Text, "", SearchOption.TopDirectoryOnly);
+
+
+            // Iteram prin toate folderele
+
+            foreach (var folder in toateFolderele)
+            {
+                FileInfo info = new FileInfo(folder);
+
+                var item = new ListViewItem(new[] { info.Name, "", "<DIR>", info.LastWriteTime.ToString() });
+
+                item_for_image = lv_locatie_1.Items.Add(item);
+
+                // Setam imaginea itemului bazat pe index (am folosit index si nu cheia direct deoarece era o problema de renderare la start-ul
+                // programului la care nu am gasit fix
+
+                item_for_image.ImageIndex = imageList.Images.IndexOfKey("folderImage");
+            }
+
+            // Iteram prin toate fisierele
+
+            foreach (var fisier in toateFisierele)
+            {
+                FileInfo info = new FileInfo(fisier);
+
+                // Daca icoana nu exista deja in lista noastra de imagini, o adaugam
+
+                if (imageList.Images.ContainsKey(info.Extension + "Image") == false)
+                    imageList.Images.Add(info.Extension + "Image", Icon.ExtractAssociatedIcon(info.FullName));
+
+                var item = new ListViewItem(new[] { info.Name, info.Extension, info.Length.ToString(), info.LastWriteTime.ToString() });
+
+                item_for_image = lv_locatie_1.Items.Add(item);
+                item_for_image.ImageIndex = imageList.Images.IndexOfKey(info.Extension + "Image");
+            }
+
+        }
+
+        // Functia asta este copiata de la textbox-ul pentru prima locatie tb_path_1 (cea din partea stanga a window-ului)
+        private void tb_path_2_TextChanged(object sender, EventArgs e)
+        {
+
+            
+
+            lv_locatie_2.Items.Clear();
+
+            var imageList = new ImageList();
+
+            imageList.Images.Add("folderImage", Image.FromFile("folderIcon.ico"));
+            imageList.Images.Add("gobackfolderImage", Image.FromFile("gobackfolderIcon.png"));
+
+            ListViewItem item_for_image;
+
+            if (Directory.GetParent(tb_path_2.Text) != null)
+            {
+                var itemul_mers_inapoi = new ListViewItem(new[] { "[...]", "<DIR>", "", "" });
+                item_for_image = lv_locatie_2.Items.Add(itemul_mers_inapoi);
+                item_for_image.ImageIndex = imageList.Images.IndexOfKey("gobackfolderImage");
+            }
+
+            imageList.ColorDepth = ColorDepth.Depth32Bit;
+            lv_locatie_2.SmallImageList = imageList;
+
+
+            string[] toateFolderele = Directory.GetDirectories(tb_path_2.Text);
+            string[] toateFisierele = Directory.GetFiles(tb_path_2.Text, "", SearchOption.TopDirectoryOnly);
+
+
+            foreach (var folder in toateFolderele)
+            {
+                FileInfo info = new FileInfo(folder);
+
+                var item = new ListViewItem(new[] { info.Name, "", "<DIR>", info.LastWriteTime.ToString() });
+
+                item_for_image = lv_locatie_2.Items.Add(item);
+
+                item_for_image.ImageIndex = imageList.Images.IndexOfKey("folderImage");
+            }
+
+
+            foreach (var fisier in toateFisierele)
+            {
+                FileInfo info = new FileInfo(fisier);
+
+                if (imageList.Images.ContainsKey(info.Extension + "Image") == false)
+                    imageList.Images.Add(info.Extension + "Image", Icon.ExtractAssociatedIcon(info.FullName));
+
+                var item = new ListViewItem(new[] { info.Name, info.Extension, info.Length.ToString(), info.LastWriteTime.ToString() });
+
+                item_for_image = lv_locatie_2.Items.Add(item);
+                item_for_image.ImageIndex = imageList.Images.IndexOfKey(info.Extension + "Image");
+            }
+
+        }
+
+        private void lv_locatie_1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Verificam daca este selectat doar un item
+            if (lv_locatie_1.SelectedItems.Count == 1)
+                // Daca are imaginea de folder sau cea pentru item-ul de redirectionare la folder-ul parinte => este un folder si trebuie intrat in el
+                if (lv_locatie_1.SelectedItems[0].ImageIndex == 0 || lv_locatie_1.SelectedItems[0].ImageIndex == 1)
+                {
+                    if (lv_locatie_1.Items.IndexOf(lv_locatie_1.SelectedItems[0]) != 0)
+                        tb_path_1.Text = tb_path_1.Text + lv_locatie_1.SelectedItems[0].Text + "/";
+                    else
+                    {
+                        if (Directory.GetParent(tb_path_1.Text.Substring(0, tb_path_1.Text.Length - 2)).FullName != cb_partitie_1.Text)
+                            tb_path_1.Text = Directory.GetParent(tb_path_1.Text.Substring(0, tb_path_1.Text.Length - 2)).FullName + "/";
+                        else
+                            tb_path_1.Text = cb_partitie_1.Text;
+                    }
+                }
+                // Daca nu are imaginea de folder sau cea pentru item-ul de redirectionare la folder-ul parinte => este un fisier si trebuie sa fie deschis
+                else
+                {
+                    var p = new System.Diagnostics.Process();
+                    p.StartInfo = new System.Diagnostics.ProcessStartInfo(tb_path_1.Text + lv_locatie_1.SelectedItems[0].Text)
+                    {
+                        UseShellExecute = true
+                    };
+                    p.Start();
+                }
+        }
+
+        private void lv_locatie_2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            if (lv_locatie_2.SelectedItems.Count == 1)
+                if (lv_locatie_2.SelectedItems[0].ImageIndex == 0 || lv_locatie_2.SelectedItems[0].ImageIndex == 1)
+                {
+                    if (lv_locatie_2.Items.IndexOf(lv_locatie_2.SelectedItems[0]) != 0)
+                        tb_path_2.Text = tb_path_2.Text + lv_locatie_2.SelectedItems[0].Text + "/";
+                    else
+                    {
+                        if (Directory.GetParent(tb_path_2.Text.Substring(0, tb_path_2.Text.Length - 2)).FullName != cb_partitie_2.Text)
+                            tb_path_2.Text = Directory.GetParent(tb_path_2.Text.Substring(0, tb_path_2.Text.Length - 2)).FullName + "/";
+                        else
+                            tb_path_2.Text = cb_partitie_2.Text;
+                    }
+                }
+                else
+                {
+                    var p = new System.Diagnostics.Process();
+                    p.StartInfo = new System.Diagnostics.ProcessStartInfo(tb_path_2.Text + lv_locatie_2.SelectedItems[0].Text)
+                    {
+                        UseShellExecute = true
+                    };
+                    p.Start();
+                }
+        }
+    }
+}
